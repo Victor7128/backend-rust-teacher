6f435ef3e069b523b0a2c143770be411f6618b13
Victor <tuestareynavictorhugo@gmail.com>
2025-07-22 13:41:58 -0500

    add: alumnos

--- src/models/alumno.rs
+++ src/models/alumno.rs
@@ -1,11 +1,13 @@
 use serde::{Deserialize, Serialize};
 use sqlx::FromRow;
 use uuid::Uuid;
-use chrono::{DateTime, Utc};
+use chrono::NaiveDateTime;
 
 #[derive(Debug, Serialize, Deserialize, FromRow)]
 pub struct Alumno {
     pub id: Uuid,
     pub nombre: String,
-    pub seccion_id: Uuid
+    pub seccion_id: Uuid,
+    pub creado_en: NaiveDateTime,
+    pub actualizado_en: NaiveDateTime,
 }
